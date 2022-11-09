9dbcb00e0efe59bf2dfef964299c64fdf0513885
김세환 <115784029+KS2H@users.noreply.github.com>
2022-11-09 23:52:50 +0900

    ✨add login skill

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
+# Account
+op_id = (open('id.txt', 'r')).readlines(0)[0]
+op_pw = (open('password.txt', 'r')).readlines(0)[0]
 
 # Chrome Driver
 chrome_options = webdriver.ChromeOptions()
@@ -10,6 +15,9 @@ chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.implicitly_wait(5)
 
+# Login
 driver.get('https://talk.op.gg/s/lol/all?sort=top')
-
 driver.find_element(By.XPATH, '//*[@id="header"]/header/div[2]/div[1]/a').click()
+driver.find_element(By.NAME, 'email').send_keys(op_id)
+time.sleep(0.2)
+driver.find_element(By.NAME, 'password').send_keys(op_pw)
