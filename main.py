49b2f3355d5d8043e6eea9787bd89ec4db863d71
김세환 <115784029+KS2H@users.noreply.github.com>
2022-11-12 22:01:29 +0900

    :sparkles:Never Start Chrome

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ op_pw = (open('password.txt', 'r')).readlines(0)[0]
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+chrome_options.add_argument('headless')
+chrome_options.add_argument('window-size=1920x1080')
+chrome_options.add_argument("disable-gpu")
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.implicitly_wait(5)
 
